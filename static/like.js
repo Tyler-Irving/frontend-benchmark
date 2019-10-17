fed892dd91170c3ea31479a5c2356302c0e174ab
Student <student@donation.local>
2019-10-17 16:10:46 -0500

    Frontend Benchmark:
    Changed the URL in the js.

--- static/like.js
+++ static/like.js
@@ -1,16 +1,14 @@
 const likeButtons = document.querySelectorAll("#likes")
-console.log(likeButtons)
+const item = document.querySelectorAll("#box")
 
 for (const button of likeButtons) {
     const plus = button.querySelector("#like-butt");
-    console.log(plus)
-    plus.addEventListener('click', function () {
-        const url = `/entries/${plus}:id/like`
+    plus.addEventListener('click', () => {
+        const url = "/entries/entry.likes/like"
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 const number = data.querySelector("#count")
-                console.log(number)
                 number.textContent = Number(number.textContent) + 1;
             })
     })
